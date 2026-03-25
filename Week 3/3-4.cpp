3fea2cb66efb04a70a47fbd71f23a695a96c26d9
ththfql <145094239+ththfql@users.noreply.github.com>
2026-03-25 15:19:43 +0900

    example

--- Week 3/3-4.cpp
+++ Week 3/3-4.cpp
@@ -12,23 +12,23 @@ int main(void) {
 
     vector<string> words;
     string a;
-
     
+
     while (cin >> a) {
         words.push_back(a);
     }
 
-   
+
     if (words.empty()) {
         cout << "입력된 단어 없음." << endl;
         return 1;
     }
-
  
+
     string::size_type max_len = words[0].size();
     string::size_type min_len = words[0].size();
+  
 
-   
     for (size_t i = 1; i < words.size(); ++i) {
         string::size_type current_len = words[i].size();
 
@@ -39,8 +39,8 @@ int main(void) {
             min_len = current_len;
         }
     }
-
     
+
     cout << "가장 긴 단어 길이: " << max_len << endl;
     cout << "가장 짧은 단어 길이: " << min_len << endl;
 
